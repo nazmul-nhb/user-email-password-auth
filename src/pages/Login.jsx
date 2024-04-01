1bba5c4b71b4daf9313aca2651ff469db881261f
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-04-02 00:51:26 +0600

    adde password reset functionalities

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,66 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebase.config";
+import { useRef, useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
 
-    const handleLogin =(e) => {
+    const [loginError, setLoginError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const emailRef = useRef(null);
+
+    const handleLogin = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
+
+        setLoginError('');
+        setSuccess('');
+
         signInWithEmailAndPassword(auth, email, password)
             .then(result => {
                 console.log(result.user);
+                setSuccess('User Logged in Successfully!')
             })
             .catch(error => {
-                console.error(error);
-            })
+                setLoginError(error.message);
+            });
+        e.target.reset();
     }
+
+    const handleForgotPassword = () => {
+        setLoginError('');
+        const email = emailRef.current.value;
+        if (!email) {
+            setLoginError('Please, Provide an Email');
+            return;
+        }
+        sendPasswordResetEmail(auth, email)
+            .then(() => setSuccess('Password Reset Email Sent Successfully!'))
+            .catch(error => setLoginError(error.message))
+    }
+
     return (
         <section className="space-y-6 flex flex-col justify-center items-center mt-8">
             <h2 className="text-2xl font-medium">Please, Login</h2>
             <form onSubmit={handleLogin} className="flex flex-col gap-6">
-                <input type="email" name="email" placeholder="Your Email" />
-                <input type="password" name="password" placeholder="Your Password" />
+                <input className="input border border-red-950" type="email" name="email" ref={emailRef} placeholder="Your Email" required />
+                <div className="relative">
+                    <input className="input w-full border border-red-950" type={showPassword ? "text" : "password"} name="password" placeholder="Your Password" required />
+                    <span className="absolute top-1/2 right-2 -translate-y-1/2" onClick={() => setShowPassword(!showPassword)}>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                </div>
+                <a onClick={handleForgotPassword} href="#">Forgot Password?</a>
                 <input type="submit" value="Login" className="btn btn-primary" />
             </form>
+            {
+                loginError && <p className="text-red-700">{loginError}</p>
+            }
+            {
+                success && <p className="text-green-700">{success}</p>
+            }
+            <p className="font-medium">New to this site? Please, <Link to={'/register'}>Register Here!</Link></p>
         </section>
     );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,17 +2,19 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebase.config";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Register = () => {
 
     const [registerError, setRegisterError] = useState('');
     const [success, setSuccess] = useState('');
-    const [showPassword, setShowPassword] = useState(false)
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleRegister = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
+        const accepted = e.target.terms.checked;
 
         setRegisterError('');
         setSuccess('');
@@ -25,6 +27,9 @@ const Register = () => {
             return;
         } else if (!/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password)) {
             setRegisterError('Your Password Should Contain at least one special character!');
+            return;
+        } else if (!accepted) {
+            setRegisterError('Please, Accept Our Terms & Conditions')
             return;
         }
 
@@ -45,8 +50,12 @@ const Register = () => {
             <form onSubmit={handleRegister} className="flex flex-col gap-6">
                 <input className="input border border-red-950" type="email" name="email" placeholder="Your Email" required />
                 <div className="relative">
-                    <input className="input border border-red-950" type={showPassword ? "text" : "password"} name="password" placeholder="Your Password" required />
+                    <input className="input w-full border border-red-950" type={showPassword ? "text" : "password"} name="password" placeholder="Your Password" required />
                     <span className="absolute top-1/2 right-2 -translate-y-1/2" onClick={() => setShowPassword(!showPassword)}>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                </div>
+                <div className="flex gap-2">
+                    <input type="checkbox" name="terms" id="terms" />
+                    <label htmlFor="terms">Accept Our <Link>Terms & Conditions</Link></label>
                 </div>
                 <input type="submit" value="Register" className="btn btn-primary" />
             </form>
@@ -56,6 +65,7 @@ const Register = () => {
             {
                 success && <p className="text-green-700">{success}</p>
             }
+            <p className="font-medium">Already have an Account? Please, <Link to={'/login'}>Login Here!</Link></p>
         </section>
     );
 };

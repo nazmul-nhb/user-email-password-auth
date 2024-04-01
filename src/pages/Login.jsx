905d01cad643461ff3dbe0f22f58fad461fb067c
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-04-01 23:08:23 +0600

    enabled login handler

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,28 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../firebase.config";
 
 const Login = () => {
+
+    const handleLogin =(e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
     return (
-        <section>
-            
+        <section className="space-y-6 flex flex-col justify-center items-center mt-8">
+            <h2 className="text-2xl font-medium">Please, Login</h2>
+            <form onSubmit={handleLogin} className="flex flex-col gap-6">
+                <input type="email" name="email" placeholder="Your Email" />
+                <input type="password" name="password" placeholder="Your Password" />
+                <input type="submit" value="Login" className="btn btn-primary" />
+            </form>
         </section>
     );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,27 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../firebase.config";
+import { useState } from "react";
 
 const Register = () => {
+
+    const [showError, setShowError] = useState('')
 
     const handleRegister = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.error(error);
+            })
     }
 
     return (
-        <section className="space-y-6 flex flex-col justify-center items-center">
-            <h2>Please, Register</h2>
+        <section className="space-y-6 flex flex-col justify-center items-center mt-8">
+            <h2 className="text-2xl font-medium">Please, Register</h2>
             <form onSubmit={handleRegister} className="flex flex-col gap-6">
                 <input type="email" name="email" placeholder="Your Email" />
                 <input type="password" name="password" placeholder="Your Password" />

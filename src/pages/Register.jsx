50b3da53916e6dc099bf64b57659cc70dca3c520
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-04-02 00:01:56 +0600

    added password validation with regex and toggle show/hide password

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,32 +1,61 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebase.config";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
 
-    const [showError, setShowError] = useState('')
+    const [registerError, setRegisterError] = useState('');
+    const [success, setSuccess] = useState('');
+    const [showPassword, setShowPassword] = useState(false)
 
     const handleRegister = (e) => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
+
+        setRegisterError('');
+        setSuccess('');
+
+        if (password.length < 6) {
+            setRegisterError('Password should be 6 characters or longer');
+            return;
+        } else if (!/[A-Z]/.test(password)) {
+            setRegisterError('Your Password Should Contain at least one upper case character!');
+            return;
+        } else if (!/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password)) {
+            setRegisterError('Your Password Should Contain at least one special character!');
+            return;
+        }
+
         createUserWithEmailAndPassword(auth, email, password)
             .then(result => {
                 console.log(result.user);
+                setSuccess('User Created Successfully!')
             })
             .catch(error => {
-                console.error(error);
-            })
+                setRegisterError(error.message);
+            });
+        e.target.reset();
     }
 
     return (
         <section className="space-y-6 flex flex-col justify-center items-center mt-8">
             <h2 className="text-2xl font-medium">Please, Register</h2>
             <form onSubmit={handleRegister} className="flex flex-col gap-6">
-                <input type="email" name="email" placeholder="Your Email" />
-                <input type="password" name="password" placeholder="Your Password" />
+                <input className="input border border-red-950" type="email" name="email" placeholder="Your Email" required />
+                <div className="relative">
+                    <input className="input border border-red-950" type={showPassword ? "text" : "password"} name="password" placeholder="Your Password" required />
+                    <span className="absolute top-1/2 right-2 -translate-y-1/2" onClick={() => setShowPassword(!showPassword)}>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
+                </div>
                 <input type="submit" value="Register" className="btn btn-primary" />
             </form>
+            {
+                registerError && <p className="text-red-700">{registerError}</p>
+            }
+            {
+                success && <p className="text-green-700">{success}</p>
+            }
         </section>
     );
 };
